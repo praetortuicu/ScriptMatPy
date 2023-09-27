7a2d32caefb1fdfaedb7f788fba2153d1ba4ad3f
praetortuicu <teodor.ticu@gmail.com>
2023-09-27 16:47:13 +0200

    Made improvement to the import from Matlab to Python

--- PythonMatlabScript/PythonMatlabScript.py
+++ PythonMatlabScript/PythonMatlabScript.py
@@ -1,8 +1,21 @@
+import h5py
 import numpy as np
 import scipy.io
-from show_data import show_data
-#path to the .mat file on your machine
-struct = scipy.io.loadmat('C:\Workspace\...\MAT_normalizedData_PostStrokeAdults_SMALL.mat')
-print(struct.keys())
+import matplotlib
 
-show_data(struct)
+
+from show_data import show_data
+from MatToPy import is_hdf5_matlab_file
+
+#path to the .mat file on your machine
+file_path = 'C:\...\matlab_file_on_your_machine.mat'
+is_hdf5_matlab_file(file_path)
+
+if is_hdf5_matlab_file(file_path) is False:
+    try:
+        struct = scipy.io.loadmat(file_path)
+        show_data(struct)
+    except OSError:
+        print("Could not load file! Check your file path, version...")
+        
+
